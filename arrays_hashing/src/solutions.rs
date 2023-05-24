62c475894b5e65161224ca928083d88996131d8f
u87612gg <george.grainger-3@student.manchester.ac.uk>
2023-05-24 16:14:19 +0100

    Realise insert returns if item exists in set

--- arrays_hashing/src/solutions.rs
+++ arrays_hashing/src/solutions.rs
@@ -2,11 +2,10 @@ use std::collections::HashSet;
 
 pub fn contains_duplicate(nums: Vec<i32>) -> bool {
     let mut seen = HashSet::<i32>::new();
-    for num in nums.iter() {
-        if seen.contains(num) {
+    for num in nums {
+        if seen.insert(num) {
             return true;
         }
-        seen.insert(*num);
     }
     false
 }
