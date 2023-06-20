d13033b19ee50cec93e4e1649618b58ac83180d3
George Grainger <georgegrainger1008@gmail.com>
2023-06-20 21:37:21 +0100

    The most beautful two liner you ever did see
    Still has surplus of iterating over the elements twice thought

--- two_pointers/valid_palindrome.rs
+++ two_pointers/valid_palindrome.rs
@@ -1,14 +1,13 @@
+#[test]
+fn test_is_palindrome() {
+    assert!(is_palindrome(String::from(
+        "A man, a plan, a canal: Panama"
+    )));
+    assert!(!is_palindrome(String::from("race a car")));
+    assert!(is_palindrome(String::from(" ")));
+}
+
 fn is_palindrome(s: String) -> bool {
-    let s = s
-        .trim()
-        .to_lowercase()
-        .replace(|c| !char::is_alphanumeric(c), "");
-
-    for (i, j) in s.chars().zip(s.chars().rev()) {
-        if (i != j) {
-            return false;
-        }
-    }
-
-    true
+    let s = s.replace(|c| !char::is_alphanumeric(c), "").to_lowercase();
+    s.chars().zip(s.chars().rev()).all(|(i, j)| i == j)
 }
