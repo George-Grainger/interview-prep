06b63188de69ec21eb0bb34f93a79023915cd0a8
George Grainger <georgegrainger1008@gmail.com>
2023-07-04 09:39:27 +0100

    Realised initial comparisons were redudant

--- two_pointers/max_area.rs
+++ two_pointers/max_area.rs
@@ -9,6 +9,7 @@ fn test_max_area() {
     assert_eq!(max_area(height), 1);
 }
 
+#[inline(always)]
 fn get_area(left: usize, right: usize, height: &Vec<i32>) -> i32 {
     let w = (right - left) as i32;
     let h = cmp::min(height[left], height[right]);
@@ -21,26 +22,11 @@ fn max_area(height: Vec<i32>) -> i32 {
     let mut area = get_area(start, end, &height);
 
     while start < end {
-        let li_area = get_area(start + 1, end, &height);
-        if li_area > area {
-            area = li_area;
-            start += 1;
-            continue;
-        }
-
-        let ri_area = get_area(start, end - 1, &height);
-        if ri_area > area {
-            area = ri_area;
-            end -= 1;
-            continue;
-        }
-
         if height[start] < height[end] {
             start += 1;
         } else {
             end -= 1;
         }
+        area = cmp::max(area, get_area(start, end, &height));
     }
-
-    area
 }
