9c160fce093fe5962e31efe6f915328c1224f077
George Grainger <georgegrainger1008@gmail.com>
2023-10-04 12:31:03 +0100

    Improved solution based on floyds algorithm

--- linked_list/find_duplicate.rs
+++ linked_list/find_duplicate.rs
@@ -1,13 +1,19 @@
-fn find_duplicate(nums: Vec<i32>) -> i32 {
-    let mut seen = vec![false; nums.len()];
+// Solution for: https://leetcode.com/problems/find-the-duplicate-number/
 
-    for num in nums {
-        if (seen[num as usize] == true) {
-            return num;
-        } else {
-            seen[num as usize] = true;
-        }
+fn find_duplicate(nums: Vec<i32>) -> i32 {
+    let mut slow = nums[0] as usize;
+    let mut fast = nums[slow] as usize;
+
+    while fast != slow {
+        slow = nums[slow] as usize;
+        fast = nums[nums[fast] as usize] as usize;
     }
 
-    0
+    slow = 0;
+    while fast != slow {
+        slow = nums[slow] as usize;
+        fast = nums[fast] as usize;
+    }
+
+    slow as i32
 }
