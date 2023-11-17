b7cc14183ba310b2d33d2b5d7748426e526b3765
George Grainger <georgegrainger1008@gmail.com>
2023-11-17 16:26:31 +0000

    Alternate solution to roman to integer

--- arrays_hashing/roman_to_integer.rs
+++ arrays_hashing/roman_to_integer.rs
@@ -1,50 +1,22 @@
-fn roman_to_int(mut s: String) -> i32 {
-    s.push('\0');
+fn roman_to_int(s: String) -> i32 {
     let mut total = 0;
-    let mut skip = false;
-    let chars: Vec<char> = s.chars().collect();
-
-    for pair in chars.windows(2) {
-        if skip {
-            skip = false;
-            continue;
-        }
-
-        match pair {
-            ['C', 'M'] => {
-                total += 900;
-                skip = true;
-            }
-            ['C', 'D'] => {
-                total += 400;
-                skip = true;
-            }
-            ['X', 'C'] => {
-                total += 90;
-                skip = true;
-            }
-            ['X', 'L'] => {
-                total += 40;
-                skip = true;
-            }
-            ['I', 'X'] => {
-                total += 9;
-                skip = true;
-            }
-            ['I', 'V'] => {
-                total += 4;
-                skip = true;
-            }
-            ['M', _] => total += 1000,
-            ['D', _] => total += 500,
-            ['C', _] => total += 100,
-            ['L', _] => total += 50,
-            ['X', _] => total += 10,
-            ['V', _] => total += 5,
-            ['I', _] => total += 1,
-            _ => unreachable!("Shouldn't get here"),
+    let mut last_val: i32 = 0;
+    for character in s.chars() {
+        let val = match character {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
         };
+        if val > last_val {
+            total -= 2 * last_val;
+        }
+        last_val = val;
+        total += val;
     }
-
     total
 }
