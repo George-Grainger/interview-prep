c40010dc14d88698deeaa35cdf01052129b16e3f
George Grainger <georgegrainger1008@gmail.com>
2023-11-17 18:22:38 +0000

    Second solution for finding longest palindrome

--- dynamic_programming/longest_palindrome.rs
+++ dynamic_programming/longest_palindrome.rs
@@ -1,29 +1,33 @@
-fn longest_palindrome(s: String) -> String {
-    let n = s.len();
-    let chars: Vec<char> = s.chars().collect();
-    let mut dp = vec![vec![false; s.len()]; s.len()];
-    let mut ans: (usize, usize) = (0, 0);
+impl Solution {
+    fn expand(i: usize, j: usize, chars: &[u8]) -> usize {
+        let mut left = i;
+        let mut right = j;
 
-    for i in 0..n {
-        dp[i][i] = true;
-    }
-
-    for (i, s) in s.as_bytes().windows(2).enumerate() {
-        if s[0] == s[1] {
-            dp[i][i + 1] = true;
-            ans = (i, 1);
+        while left as isize >= 0 && right < chars.len() && chars[left] == chars[right] {
+            left -= 1;
+            right += 1;
         }
+
+        right - left - 1
     }
 
-    for diff in 2..n {
-        for i in 0..(n - diff) {
-            let j = i + diff;
-            if chars[i] == chars[j] && dp[i + 1][j - 1] {
-                dp[i][j] = true;
-                ans = (i, diff)
+    pub fn longest_palindrome(s: String) -> String {
+        let mut ans = [0, 0];
+
+        for i in 0..s.len() {
+            let odd_len = Self::expand(i, i, &s.as_bytes());
+            if (odd_len > ans[1]) {
+                let dist = odd_len / 2;
+                ans = [i - dist, odd_len];
+            }
+
+            let even_len = Self::expand(i, i + 1, &s.as_bytes());
+            if (even_len > ans[1]) {
+                let dist = (even_len / 2) - 1;
+                ans = [i - dist, even_len]
             }
         }
-    }
 
-    s.chars().skip(ans.0).take(ans.1 + 1).collect()
+        s.chars().skip(ans[0]).take(ans[1]).collect()
+    }
 }
