cd83e32254e086a54dc3e991e15f72e551c8fff8
George Grainger <georgegrainger1008@gmail.com>
2023-11-27 10:03:30 +0000

    Found cleaner solution

--- backtracking/subsets_with_duplicate.rs
+++ backtracking/subsets_with_duplicate.rs
@@ -1,26 +1,22 @@
-use std::collections::HashSet;
+/// Solution for: https://leetcode.com/problems/subsets-ii/
 
-impl Solution {
-    fn _ss(nums: &[i32], seen: &mut HashSet<i32>, res: &mut Vec<Vec<i32>>) {
-        if nums.len() == 1 {
-            let initial_use = seen.insert(nums[0]);
-            for mut subset in res.clone() {
-                subset.push(nums[0]);
-                if initial_use || !res.contains(&subset) {
-                    res.push(subset)
-                }
-            }
-        } else {
-            Self::_ss(&nums[0..1], seen, res);
-            Self::_ss(&nums[1..], seen, res);
+fn subsets_with_dup(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut result = Vec::new();
+    nums.sort();
+    backtrack(&nums, 0, &mut Vec::new(), &mut result);
+    result
+}
+
+fn backtrack(nums: &[i32], start: usize, subset: &mut Vec<i32>, result: &mut Vec<Vec<i32>>) {
+    result.push(subset.clone());
+
+    for i in start..nums.len() {
+        if i > start && nums[i] == nums[i - 1] {
+            continue;
         }
-    }
 
-    pub fn subsets_with_dup(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
-        nums.sort();
-        let mut res = vec![vec![]];
-        let mut seen = HashSet::new();
-        Self::_ss(&nums[..], &mut seen, &mut res);
-        res
+        subset.push(nums[i]);
+        backtrack(nums, i + 1, subset, result);
+        subset.pop();
     }
 }
