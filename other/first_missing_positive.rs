b1066c735e3bf9664a846085367829a029df355e
George Grainger <georgegrainger1008@gmail.com>
2023-12-04 11:14:40 +0000

    Attempt using summation
    Unfortunatley doesn't work when duplicates or large gaps

--- other/first_missing_positive.rs
+++ other/first_missing_positive.rs
@@ -1,16 +1,34 @@
 /// Naive solution for: https://leetcode.com/problems/first-missing-positive/
 
-use std::collections::HashSet;
-use std::iter::FromIterator;
-
 impl Solution {
     pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
-        let nums: HashSet<i32> = HashSet::from_iter(nums.into_iter());
-        
-        let mut i = 1;
-        while nums.contains(&i) {
-            i += 1;
+        let mut lowest = i32::MAX;
+        let mut highest = 0;
+        let mut sum = 0;
+
+        for num in nums {
+            if num > 0 {
+                lowest = lowest.min(num);
+                highest = highest.max(num);
+                sum += num;
+            }
         }
-        i
+
+        // If 1 hasn't been seen return this
+        if lowest != 1 {
+            return 1;
+        }
+
+        // Calculate difference between expected sum and seen sum
+        let expected_sum = (highest) * (highest + 1) / 2;
+        let diff = expected_sum - sum;
+
+        // If there's no difference the next value is the first missing
+        // Otherwise the difference is the missing number
+        if diff == 0 {
+            highest + 1
+        } else {
+            diff
+        }
     }
 }
